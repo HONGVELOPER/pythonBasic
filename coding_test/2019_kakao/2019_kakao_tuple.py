a32cc813a840b077389a7be0357fe7fadc589a24
Young-Jin1003 <hyjinn1003@naver.com>
2021-05-24 15:12:27 +0900

    2019 kakao tuple

--- coding_test/2019_kakao/2019_kakao_tuple.py
+++ coding_test/2019_kakao/2019_kakao_tuple.py
@@ -1,10 +1,23 @@
-s = '{{2}, {2, 1}, {2, 1, 3}, {2, 1, 3, 4}}'
-# s = '{{20,111},{111}}'
 
 def solution(s):
     answer = []
-    clean = ''
-    a = set()
+    final = []
+    s = s[2:-2].split('},{')
+    print(s)
+    for i in s:
+        a = [int(temp) for temp in i.split(',')]
+        answer.append(a)
+    print(answer)
+    answer = sorted(answer, key=len)
+    for i in answer:
+        for j in i:
+            if j not in final:
+                final.append(j)
+    print(final, 'final!')
+    return final
 
-    
+s = "{{4,2,3},{3},{2,3,4,1},{2,3}}"
+# s = "{{20,111},{111}}"
+# s = "{{2},{2,1},{2,1,3},{2,1,3,4}}"
+
 solution(s)
